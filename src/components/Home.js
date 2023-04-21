99042299641a056d3ab74d412d12038f4c8c3c38
Akash Kumar <akash.01raj@gmail.com>
2023-04-21 18:19:33 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import homeImg from "../assets/home/background-home-desktop.jpg"
+
+export default function Home() {
+  return (
+    <div className='homepage'>
+        <img src={homeImg} id='home-img' alt='earth-backbround' width={"100%"} height={"100%"}/>
+        <div className='home-parent'>
+            <div className='child-space'>
+                <h4>SO, YOU WANT TO TRAVEL TO</h4>
+                <h1>SPACE</h1>
+                <p>Let’s face it; if you want to go to space, you might as well genuinely 
+                    go to outer space and not hover kind of on the edge of it. Well sit 
+                    back, and relax because we’ll give you a truly out of this world experience!
+                </p>
+            </div>
+            <div className='child-explore'>
+                <span className='explore'>EXPLORE</span>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='navbar'>
+        <ul>
+            <li>Home</li>
+            <li>Destination</li>
+            <li>Crew</li>
+            <li>TECHNOLOGY</li>
+        </ul>
+
+    </div>
+  )
+}
