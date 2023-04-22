20d576c63dfecb3d7148d3f79fef42038a6a8f6c
Akash Kumar <akash.01raj@gmail.com>
2023-04-22 23:01:57 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Destination from './components/Destination';
+import Crew from './components/Crew';
+import Technology from './components/Technology';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Home />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/destination' element={<Destination />}/>
+          <Route path='/crew' element={<Crew />}/>
+          <Route path='/technology' element={<Technology />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import starLogo from "../assets/shared/logo.svg"
 
 export default function Nav() {
   return (
     <div className='navbar'>
+        <div className='icon-line'>
+          <img src={starLogo} alt='star-logo' />
+          <hr />
+        </div>
         <ul>
-            <li>Home</li>
-            <li>Destination</li>
-            <li>Crew</li>
-            <li>TECHNOLOGY</li>
+          <li><Link to="/"><a ><strong>00</strong> home</a></Link></li>
+          <li><Link to="/destination"><a ><strong>01</strong> destination</a></Link></li>
+          <li><Link to="/crew"><a ><strong>02</strong> crew</a></Link></li>
+          <li><Link to="/technology"><a ><strong>03</strong> technology</a></Link></li>
         </ul>
 
     </div>
